4b022fe9dfac470e221939645b61cc13e260ff1f
williamkiddy <hei.s.fue@gmail.com>
2025-02-04 13:37:41 +0100

    feat: Updated colors

--- lua/kiddy/colors/init.lua
+++ lua/kiddy/colors/init.lua
@@ -5,27 +5,27 @@ function C.extend_palette(options)
 	C.none = "NONE"
 	-- Backgrounds
 	C.bg = C.base0                              -- Main background
-	C.bg_dark = C.base1                         -- Darker background for inactive areas
+	C.bg_dark = C.sec1                          -- Darker background for inactive areas
 	C.bg_inactive = C.base2                     -- Background for inactive windows
-	C.bg_highlight = U.blend(C.sec0, C.base0, 0.2) -- Highlighted background
-	C.bg_visual = C.accent0                     -- for highlighting (cursor)
+	C.bg_highlight = U.blend(C.sec1, C.base0, 0.2) -- Highlighted background
+	C.bg_visual = C.base3                       -- for highlighting (cursor)
 	C.bg_bright = U.lighten(C.pastel8, 0.2, C.bg) -- Bright background
-	C.bg_sidebar = C.base6                      -- Sidebar background
+	C.bg_sidebar = C.sec10                      -- Sidebar background
 	C.bg_statusline = C.none                    -- Statusline background
 	C.bg_selected = U.blend(C.bg, C.def8, 0.88) -- Background for selected items
 	C.bg_fold = C.base2                         -- Background for folded sections
 
 	-- Foregrounds
-	C.fg = C.base7                                -- Main foreground (text)
-	C.fg_dark = C.base6                           -- Darker text
-	C.fg_inactive = C.sec8                        -- Inactive window text
-	C.fg_highlight = C.sec2                       -- Highlighted text
-	C.fg_bright = U.lighten(C.base7, 0.8, C.pastel8) -- Bright foreground (text)
-	C.fg_visual = C.tone2                         -- Visual mode text
-	C.fg_sidebar = C.sec8                         -- Sidebar text
-	C.fg_statusline = C.sec7                      -- Statusline text
-	C.fg_selected = C.sec15                       -- Text for selected items
-	C.fg_fold = C.base4                           -- Text for folded sections
+	C.fg = C.base2                             -- Main foreground (text)
+	C.fg_dark = U.blend(C.base2, C.sec14, 0.144) -- Darker text
+	C.fg_inactive = C.sec8                     -- Inactive window text
+	C.fg_highlight = C.sec2                    -- Highlighted text
+	C.fg_bright = U.lighten(C.base8, 0.8, C.def8) -- Bright foreground (text)
+	C.fg_visual = C.sec6                       -- Visual mode text
+	C.fg_sidebar = C.base2                     -- Sidebar text
+	C.fg_statusline = C.sec7                   -- Statusline text
+	C.fg_selected = C.accent3                  -- Text for selected items
+	C.fg_fold = C.base4                        -- Text for folded sections
 
 	if options.theme == "Mufflora" then
 		C.bg = C.sec0
@@ -33,14 +33,14 @@ function C.extend_palette(options)
 	end
 
 	-- Popups
-	C.bg_popup = C.sec8      -- Popup background
-	C.fg_popup = C.tone1     -- Popup text
-	C.bg_popup_border = C.base1 -- Popup border background
-	C.fg_popup_border = C.sec3 -- Popup border text
+	C.bg_popup = U.blend(C.bg, C.base8, 0.144) -- Popup background
+	C.fg_popup = U.lighten(C.fg, 0.2, C.bg)  -- Popup text
+	C.bg_popup_border = C.base1              -- Popup border background
+	C.fg_popup_border = C.sec3               -- Popup border text
 
 	-- Floating windows
 	C.bg_float = U.blend(C.bg, C.pastel9, 0.9) -- Background for floating windows
-	C.fg_float = C.tone3                    -- Text for floating windows
+	C.fg_float = U.blend(C.fg, C.tone3, 0.144) -- Text for floating windows
 	C.bg_float_border = C.bg_float          -- Border background for floating windows
 	C.fg_float_border = C.sec3              -- Border text for floating windows
 
