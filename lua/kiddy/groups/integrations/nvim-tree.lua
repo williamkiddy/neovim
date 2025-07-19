7a5b1e8d971ba107412ea4210c7ef9d864393ac4
williamkiddy <hei.s.fue@gmail.com>
2025-07-19 17:45:32 +0200

    Changes

--- lua/kiddy/groups/integrations/nvim-tree.lua
+++ lua/kiddy/groups/integrations/nvim-tree.lua
@@ -5,7 +5,7 @@ function M.get(colors, options)
 		NvimTreeFolderName = { fg = colors.fg },
 		NvimTreeFolderIcon = { fg = colors.sec },
 		NvimTreeCursorLine = { bg = colors.bg_selected },
-		NvimTreeNormal = { fg = colors.def4 },
+		NvimTreeNormal = { fg = colors.base1 },
 		NvimTreeOpenedFolderName = { link = "NvimTreeNormal" },
 		NvimTreeEmptyFolderName = { fg = U.blend(colors.fg, colors.bg, 0.144) },
 		NvimTreeIndentMarker = { fg = colors.base4 },
