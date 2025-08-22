a5f74bdcec5f825643e03861a3e7926d6b6c9599
williamkiddy <hei.s.fue@gmail.com>
2025-08-22 22:06:43 +0200

    Changes

--- lua/kiddy/colors/init.lua
+++ lua/kiddy/colors/init.lua
@@ -17,7 +17,7 @@ function C.extend_palette(options)
 
 	-- Foregrounds
 	C.fg = C.pastel15 -- Main foreground (text)
-	C.fg_dark = U.blend(C.pastel15, C.base2, 0.8) -- Darker text
+	C.fg_dark = C.extra.neutral6 -- Darker text
 	C.fg_inactive = C.base1 -- Inactive window text
 	C.fg_highlight = C.pastel13 -- Highlighted text
 	C.fg_bright = U.lighten(C.base8, 0.8, C.sec10) -- Bright foreground (text)
