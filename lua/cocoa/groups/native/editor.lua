715f3a632e0d92d33066a6e91885875cab5cf422
williamkiddy <hei.s.fue@gmail.com>
2026-03-14 10:44:20 +0100

    Updated: colors

--- lua/cocoa/groups/native/editor.lua
+++ lua/cocoa/groups/native/editor.lua
@@ -1,84 +1,100 @@
 local M = {}
 local U = require("cocoa.utils")
+
 function M.get(colors, options)
 	return {
-		Comment = { fg = colors.fg_dark, italic = options.italic_comments },                         -- Comment text, italicized if configured
-		ColorColumn = { bg = colors.bg_visual },                                                     -- Color column background
-		Conceal = { fg = colors.base0 },                                                             -- Concealed text
+		Comment = { fg = colors.fg_dark, italic = options.italic_comments },
 
-		Cursor = { fg = colors.bg, bg = colors.fg },                                                 -- Cursor with swapped foreground and background
-		lCursor = { fg = colors.bg },                                                                -- Line cursor in background color
-		CursorIM = { fg = colors.fg },                                                               -- Cursor for input mode in foreground color
-		CursorColumn = { bg = U.blend(colors.bg_bright, colors.fg_visual, options.cursorline.blend) }, -- Cursor line background blended
-		CursorLine = { bg = colors.bg_highlight, bold = options.cursorline.bold },                   -- Cursor line background with optional bold
-		CursorLineNr = { fg = colors.fg_highlight, bold = options.cursorline.bold_number },          -- Cursor line number in highlight color
-		CursorLineSign = { bg = colors.def2 },                                                       -- No specific color for cursor line signs
+		ColorColumn = { bg = colors.bg_visual },
 
-		Directory = { fg = colors.fg_highlight },                                                    -- Directory names in highlight color
+		Conceal = { fg = colors.A3 },
 
-		EndOfBuffer = { fg = colors.fg_sidebar },                                                    -- End of buffer in sidebar color
+		Cursor = { fg = colors.bg, bg = colors.fg },
+		lCursor = { fg = colors.bg },
+		CursorIM = { fg = colors.fg },
 
-		ErrorMsg = { fg = colors.error },                                                            -- Error messages
+		CursorColumn = { bg = U.blend(colors.bg_bright, colors.fg_visual, options.cursorline.blend) },
 
-		VertSplit = { fg = colors.fg_inactive, bg = colors.bg },                                     -- Vertical split line with inactive foreground
-		WinSeparator = { fg = colors.fg_inactive, bg = colors.bg },                                  -- Window separator with inactive foreground Folded = { fg = colors.fg_fold, bg = colors.bg_fold }, -- Folded text FoldColumn = { bg = colors.bg_fold, fg = colors.fg_fold }, -- Fold column with background and foreground
+		CursorLine = { bg = colors.bg_highlight, bold = options.cursorline.bold },
 
-		SignColumn = { bg = colors.bg_sidebar, fg = colors.fg_sidebar },                             -- Sign column in sidebar colors
-		SignColumnSB = { bg = colors.bg_sidebar, fg = colors.fg_sidebar },                           -- Second sign column
+		CursorLineNr = { fg = colors.fg_highlight, bold = options.cursorline.bold_number },
 
-		Substitute = { bg = colors.sec2, fg = colors.bg },                                           -- Substitute text background, foreground in background color
+		CursorLineSign = { bg = colors.bg_sidebar },
 
-		LineNr = { fg = colors.fg_sidebar },                                                         -- Line numbers in sidebar color
+		Directory = { fg = colors.fg_highlight },
 
-		MatchParen = { underline = true, bold = true, sp = colors.bg_highlight },                    -- Matched parentheses in bold and underlined
-		ModeMsg = { fg = colors.fg, bold = true },                                                   -- Mode message in foreground color and bold
-		MsgArea = { fg = colors.fg },                                                                -- Message area in foreground color
-		MoreMsg = { fg = colors.sec2 },                                                              -- More messages
+		EndOfBuffer = { fg = colors.fg_sidebar },
 
-		NonText = { fg = colors.base0 },                                                             -- Non-text characters
+		ErrorMsg = { fg = colors.error },
 
-		Normal = { fg = colors.fg, bg = colors.bg },                                                 -- Normal text in foreground and background
-		NormalNC = { fg = colors.fg, bg = colors.bg },                                               -- Normal text in non-current windows
-		NormalSB = { fg = colors.fg_sidebar, bg = colors.bg_sidebar },                               -- Normal text in status bar windows
+		VertSplit = { fg = colors.fg_inactive, bg = colors.bg },
+		WinSeparator = { fg = colors.fg_inactive, bg = colors.bg },
 
-		NormalFloat = { fg = colors.fg_float, bg = colors.bg_float },                                -- Float window text
-		FloatBorder = { fg = colors.fg_float_border, bg = colors.bg_float_border },                  -- Float window border
+		Folded = { fg = colors.fg_fold, bg = colors.bg_fold },
+		FoldColumn = { bg = colors.bg_fold, fg = colors.fg_fold },
 
-		Pmenu = { bg = colors.bg_popup, fg = colors.fg_popup, blend = 80 },                          -- Popup menu
-		PmenuSel = { bg = colors.bg_selected },                                                      -- Selected popup menu item
-		PmenuSbar = { bg = colors.base1 },                                                           -- Popup menu scrollbar
-		PmenuThumb = { bg = colors.base1, fg = colors.base1 },                                       -- Popup menu scrollbar thumb
+		SignColumn = { bg = colors.bg_sidebar, fg = colors.fg_sidebar },
+		SignColumnSB = { bg = colors.bg_sidebar, fg = colors.fg_sidebar },
 
-		Folded = { bg = colors.bg_bright, fg = colors.fg },
+		Substitute = { bg = colors.C0, fg = colors.bg }, -- rose on dark bg
 
-		Question = { fg = colors.info },                                                -- Question prompt
-		QuickFixLine = { bg = colors.bg_visual, bold = true },                          -- Quick fix line
+		LineNr = { fg = colors.fg_sidebar },
 
-		Search = { bg = colors.base0, fg = colors.base3, bold = true, underline = true }, -- Search results
-		IncSearch = { bg = colors.sec2, fg = colors.base1, bold = true },               -- Incremental search
-		CurSearch = { link = "IncSearch" },                                             -- Current search style same as incremental search
+		MatchParen = { underline = true, bold = true, sp = colors.bg_highlight },
 
-		SpecialKey = { link = "NonText" },                                              -- Special keys linked to NonText style
+		ModeMsg = { fg = colors.fg, bold = true },
+		MsgArea = { fg = colors.fg },
+		MoreMsg = { fg = colors.C6 },
 
-		SpellBad = { sp = colors.error, undercurl = true },                             -- Misspelled words
-		SpellCap = { sp = colors.warning, undercurl = true },                           -- Capitalized errors
-		SpellLocal = { sp = colors.info, undercurl = true },                            -- Localized spelling errors
-		SpellRare = { sp = colors.hint, undercurl = true },                             -- Rare spelling errors
+		NonText = { fg = colors.A3 },
 
-		StatusLine = { fg = colors.fg_sidebar, bg = colors.bg_sidebar },                -- Status line in current window
-		StatusLineNC = { fg = colors.sec0, bg = colors.bg_statusline },                 -- Status line in non-current windows
-		TabLine = { bg = colors.bg_statusline, fg = colors.fg },                        -- Tab line
-		TabLineFill = { bg = colors.black0, fg = colors.none },                         -- Tab line fill
-		TabLineSel = { fg = colors.fg_bright, bg = colors.bg },                         -- Active tab line
+		Normal = { fg = colors.fg, bg = colors.bg },
+		NormalNC = { fg = colors.fg, bg = colors.bg },
+		NormalSB = { fg = colors.fg_sidebar, bg = colors.bg_sidebar },
 
-		Title = { fg = colors.fg_bright, bold = true },                                 -- Title text with bright foreground and bold
-		Visual = { bg = colors.bg_visual, bold = options.cursorline.bold },             -- Visual mode background
-		VisualNOS = { bg = colors.bg_visual },                                          -- Visual mode in "Not Owning the Selection"
-		WarningMsg = { fg = colors.warning },                                           -- Warning messages
-		Whitespace = { fg = colors.base1 },                                             -- Whitespace characters
-		WildMenu = { bg = colors.bg_visual },                                           -- Current match in wildmenu
-		WinBar = { bg = colors.bg_dark, fg = colors.fg },                               -- Window bar with dark background
-		WinBarNC = { bg = colors.bg_dark, fg = colors.fg_inactive },                    -- Non-current window bar
+		NormalFloat = { fg = colors.fg_float, bg = colors.bg_float },
+		FloatBorder = { fg = colors.fg_float_border, bg = colors.bg_float_border },
+
+		Pmenu = { bg = colors.bg_popup, fg = colors.fg_popup, blend = 80 },
+		PmenuSel = { bg = colors.bg_selected },
+		PmenuSbar = { bg = colors.A2 },
+		PmenuThumb = { bg = colors.A4 },
+
+		Question = { fg = colors.info },
+
+		QuickFixLine = { bg = colors.bg_visual, bold = true },
+
+		Search = { bg = colors.A3, fg = colors.A6, bold = true, underline = true },
+		IncSearch = { bg = colors.B3, fg = colors.A0, bold = true },
+		CurSearch = { link = "IncSearch" },
+
+		SpecialKey = { link = "NonText" },
+
+		SpellBad = { sp = colors.error, undercurl = true },
+		SpellCap = { sp = colors.warning, undercurl = true },
+		SpellLocal = { sp = colors.info, undercurl = true },
+		SpellRare = { sp = colors.hint, undercurl = true },
+
+		StatusLine = { fg = colors.fg_statusline, bg = colors.bg_statusline },
+		StatusLineNC = { fg = colors.fg_inactive, bg = colors.bg_statusline },
+
+		TabLine = { bg = colors.bg_statusline, fg = colors.fg },
+		TabLineFill = { bg = colors.A0, fg = colors.none },
+		TabLineSel = { fg = colors.fg_bright, bg = colors.bg },
+
+		Title = { fg = colors.fg_bright, bold = true },
+
+		Visual = { bg = colors.bg_visual, bold = options.cursorline.bold },
+		VisualNOS = { bg = colors.bg_visual },
+
+		WarningMsg = { fg = colors.warning },
+
+		Whitespace = { fg = colors.A2 },
+
+		WildMenu = { bg = colors.bg_visual },
+
+		WinBar = { bg = colors.bg_dark, fg = colors.fg },
+		WinBarNC = { bg = colors.bg_dark, fg = colors.fg_inactive },
 	}
 end
 
